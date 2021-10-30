6290ac902db58fa40cb1397244bb9ead6801c3c9
Lalit kumar <lalitkumarkonde2014@gmail.com>
2021-10-30 14:15:55 +0530

    added new files

---
+++ File1.js
@@ -0,0 +1,7 @@
+var number = 5;
+var number2 = 5; 
+var addition = number+number2;
+console.log("first number:" ,number);
+console.log("second number:",number2);
+console.log("addition is:", addition);
+console.log("result", number+number2);
